ebc83ecf0142d60091350b978658b2c18b207d82
vfdeoliveira <vinicius.o@aln.senaicimatec.edu.br>
2025-04-01 16:56:13 -0300

    2.1.4

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FiLinkedin, FiGithub, FiTwitter, FiMail } from 'react-icons/fi';
-import './footer.css';
+import './Footer.css';
 
 const Footer: React.FC = () => {
     return (
